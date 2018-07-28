047ed9942e6cfbd7102d890d6fcde4bcba61149a
Shashank Srivastava <shashank.ssriv@gmail.com>
2018-07-28 09:20:26 +0530

    Add income tax calculator, move JavaScript code to separate files, add share options.

---
+++ calcEMI.js
@@ -0,0 +1,68 @@
+function calc() {
+  var P = Number(document.getElementById("principal").value);
+  var R = document.getElementById("rate").value/1200;
+  var N = Number(document.getElementById("duration").value*12);
+  var EMI = P * R / (1 - (Math.pow(1/(1 + R), N)));
+  var EMIinWords = numberToWords.toWords(EMI);
+  document.getElementById("myModalLabel").innerHTML = "The EMI is : - " + EMI.toFixed(2) + " Rupees.";
+  document.getElementById("myModalLabelinWords").innerHTML = "In other words, yout EMI is : - " + EMIinWords + " Rupees(rounded off).";
+}
+
+function toWords() {
+  var P = Number(document.getElementById("principal").value);
+  var N = Number(document.getElementById("duration").value*12);
+  var principalAmount = numberToWords.toWords(P);
+  var months = numberToWords.toWords(N);
+  document.getElementById("principalAmountInput").innerHTML = "The total loan amount chosen is... " + principalAmount;
+  document.getElementById("durationInMonths").innerHTML = "The duration in months is... " + months;
+}
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(calcBreakUp);
+function calcBreakUp() {
+  var P = Number(document.getElementById("principal").value);
+  var R = document.getElementById("rate").value/1200;
+  var N = Number(document.getElementById("duration").value*12);
+  var EMI = P * R / (1 - (Math.pow(1/(1 + R), N)));
+  var totalAmount = (EMI * N);
+  console.log(totalAmount)
+  var totalInterest = (totalAmount - P);
+  console.log(totalInterest)
+  document.getElementById("totalAmountModalLabel").innerHTML = "Total amount you will pay against " + "the principal amount of Rupees " + P + " is... " + totalAmount.toFixed(2) + " Rupees";
+  document.getElementById("totalInterestModalLabel").innerHTML = "Out of which the total interest you will end up with is... " + totalInterest.toFixed(2) + " Rupees";
+  var data = google.visualization.arrayToDataTable([
+    ['Task', 'Hours per Day'],
+    ['Total Amount', totalAmount],
+    ['Total Interest',     totalInterest]
+  ]);
+  var options = {
+    title: 'EMI Breakup Chart',
+    width: 325,
+    legend: 'Amount',
+    is3D: true,
+    animation: {"startup": true},
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+
+  if (navigator.userAgent.match(/Trident\/7\./)) {
+    google.visualization.events.addListener(chart, 'click', function() {
+      chart_div.innerHTML = '<img src="' + chart.getImageURI() + '">';
+      console.log(chart_div.innerHTML);
+    });
+    chart.draw(data, options);
+  } else {
+    google.visualization.events.addListener(chart, 'select png', function() {
+      chart_div.innerHTML = '<img src="' + chart.getImageURI() + '">';
+      console.log(chart_div.innerHTML);
+    });
+    chart.draw(data, options);
+    document.getElementById('png').innerHTML = '<a href="' + chart.getImageURI() + '" target="_blank"><span class="glyphicon glyphicon-print"></span></a>';
+  }
+}
+
+function toMonths() {
+  var N = Number(document.getElementById("duration").value*12);
+  var months = numberToWords.toWords(N);
+  document.getElementById("durationInMonths").innerHTML = "The duration in months is... " + months;
+}
---
+++ calcIncomeTax.js
@@ -0,0 +1,96 @@
+function calc() {
+  var TI = Number(document.getElementById("totalIncome").value);
+  var TIA = Number(document.getElementById("additionalIncomeA").value);
+  var TIB = Number(document.getElementById("additionalIncomeB").value);
+  var TIC = Number(document.getElementById("additionalIncomeC").value);
+  var HRA = Number(document.getElementById("hra").value);
+  var CA = Number(document.getElementById("conveyance").value);
+  var GI = (TI + TIA + TIB + TIC);
+  var NI = (GI - HRA - CA);
+  var PF = Number(document.getElementById("pf").value);
+  var NSC = Number(document.getElementById("nsc").value);
+  var PPF = Number(document.getElementById("ppf").value);
+  var MF = Number(document.getElementById("mf").value);
+  var LIC = Number(document.getElementById("lic").value);
+  var HLP = Number(document.getElementById("homeloanPrincipal").value);
+  var T80C = (PF + NSC + PPF + MF + LIC + HLP);
+  if (T80C <= 150000) {
+    var T80C = T80C;
+  } else {
+    T80C = 150000;
+  }
+  var MS = Number(document.getElementById("medicalSelf").value);
+  var MP = Number(document.getElementById("medicalParents").value);
+  var TM = (MS + MP)
+  var EL = Number(document.getElementById("education").value);
+  var NPS = Number(document.getElementById("nps").value);
+  if (NPS <= 50000) {
+    var NPS = NPS;
+  } else {
+    NPS = 50000;
+  }
+  var HLI = Number(document.getElementById("homeloanInterest").value);
+  var DEDUCTIONS = (T80C + TM + EL + NPS + HLI);
+  var taxableIncome = (NI - DEDUCTIONS);
+
+  if (taxableIncome < 250000) {
+    document.getElementById("totalTax").innerHTML = "Your taxable income is less than 2,50,000 Rupees. No tax payable!";
+    return false;
+  } else if (taxableIncome >= 250000 && taxableIncome <= 500000) {
+    var netTaxpayable = ( taxableIncome - 250000);
+    var AT = (( 5 * netTaxpayable ) / 100);
+  } else if (taxableIncome >= 500000 && taxableIncome <= 1000000) {
+    var netTaxpayable = (taxableIncome - 500000);
+    var AT = (20 * netTaxpayable)/ 100 + 12500;
+  }
+  else if (taxableIncome > 1000000){
+    netTaxpayable = ( taxableIncome - 500000);
+    AT = (( 30 * taxableIncome ) / 100 ) + 100000 + 12500;
+  }
+  var cess = (3 * AT)/100;
+  var totalTax = cess + AT;
+  document.getElementById("actualTax").innerHTML = "Income Tax : "+ AT;
+  document.getElementById("cess").innerHTML = "Health and Education Cess : "+ cess.toFixed(0);
+  document.getElementById("totalTax").innerHTML = "Total Tax : "+ totalTax;
+  document.getElementById("grossIncomeFinal").innerHTML = "Total Gross Income : "+ GI;
+  document.getElementById("netIncomeFinal").innerHTML = "Net Income : "+ NI;
+  document.getElementById("totalDeductions").innerHTML = "Total Deductions : "+ DEDUCTIONS;
+  document.getElementById("incometax").innerHTML = "Taxable Income : "+ taxableIncome;
+}
+
+function netIncomeFn() {
+  var TI = Number(document.getElementById("totalIncome").value);
+  var TIA = Number(document.getElementById("additionalIncomeA").value);
+  var TIB = Number(document.getElementById("additionalIncomeB").value);
+  var TIC = Number(document.getElementById("additionalIncomeC").value);
+  var GI = (TI + TIA + TIB + TIC);
+  var HRA = Number(document.getElementById("hra").value);
+  var CA = Number(document.getElementById("conveyance").value);
+  var NI = (GI - HRA - CA);
+  var TA = (HRA + CA);
+  var PF = Number(document.getElementById("pf").value);
+  var NSC = Number(document.getElementById("nsc").value);
+  var PPF = Number(document.getElementById("ppf").value);
+  var MF = Number(document.getElementById("mf").value);
+  var LIC = Number(document.getElementById("lic").value);
+  var HLP = Number(document.getElementById("homeloanPrincipal").value);
+  var T80C = (PF + NSC + PPF + MF + LIC + HLP);
+  var MS = Number(document.getElementById("medicalSelf").value);
+  var MP = Number(document.getElementById("medicalParents").value);
+  var TM = (MS + MP)
+
+  document.getElementById("netIncome").innerHTML = NI;
+  document.getElementById("grossIncome").innerHTML = GI;
+  document.getElementById("totalAllowance").innerHTML = TA;
+  document.getElementById("total80c").innerHTML = T80C;
+  document.getElementById("totalmed").innerHTML = TM;
+}
+
+function toWords() {
+  var P = Number(document.getElementById("principal").value);
+  var N = Number(document.getElementById("duration").value*12);
+  var principalAmount = numberToWords.toWords(P);
+  var months = numberToWords.toWords(N);
+  document.getElementById("principalAmountInput").innerHTML = "The total loan amount chosen is... " + principalAmount;
+  document.getElementById("durationInMonths").innerHTML = "The duration in months is... " + months;
+}
