55f607b86be5140e187a95976880e6146a20f8df
Shashank Srivastava <cheakymickey@gmail.com>
2019-06-24 10:56:27 +0400

    Update calcIncomeTax.js
    Use Chartjs instead of Google Charts

--- calcIncomeTax.js
+++ calcIncomeTax.js
@@ -173,24 +173,39 @@ function drawInvestmentChart() {
   var mutualFunds = Number(document.getElementById("mf").value);
   var lifeInsurance = Number(document.getElementById("lic").value);
   var nationalPensionScheme = Number(document.getElementById("nps").value);
-  var data = google.visualization.arrayToDataTable([
-    ['Category', 'Amount(Rs)'],
-    ['NPS',     nationalSavingScheme],
-    ['PPF',     publicProvidentFund],
-    ['NSC',      nationalSavingScheme],
-    ['MF',  mutualFunds],
-    ['Life Insurance', lifeInsurance]
-  ]);
-
-  var options = {
-    width: 350,
-    legend: "bottom",
-    title: "Breakup Summary",
-    animation:{
-      startup: true,
-      duration: 1500,
+  var ctx = document.getElementById('barchartInvestments');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['NPS', 'PPF', 'NSC', 'MF', 'Life Insurance'],
+      datasets: [{
+        label: 'Rupees',
+        data: [nationalPensionScheme, publicProvidentFund, nationalSavingScheme, mutualFunds, lifeInsurance],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)'
+        ],
+        borderWidth: 1
+      }]
     },
-  };
-  var chart = new google.visualization.ColumnChart(document.getElementById('piechartInvestments'));
-  chart.draw(data, options);
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
 }
